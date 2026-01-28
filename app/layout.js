6fff7f8c4b3d67cc910b4f1fca621533a144a302
johqniel <daniel.nogues.daniel@gmail.com>
2026-01-28 02:11:22 +0100

    Redeploy.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { CartProvider } from "../context/CartContext";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -21,9 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <CartProvider>
-          {children}
-        </CartProvider>
+        {children}
       </body>
     </html>
   );
